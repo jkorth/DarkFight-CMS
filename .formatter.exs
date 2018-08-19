933a782477aa5ddef81acd12941878f737084f66
jkorth <jake@modinfinity.com>
2018-08-19 15:11:46 -0500

    Add formatter config

---
+++ .formatter.exs
@@ -0,0 +1,13 @@
+[
+  inputs: [
+    "mix.exs",
+    "lib/**/*.ex",
+    "lib/**/*.exs",
+    "config/**/.ex",
+    "config/**/.exs",
+    "test/**/*.ex",
+    "test/**/*.exs",
+  ],
+  line_length: 80,
+  rename_deprecated_at: "1.7"
+]
