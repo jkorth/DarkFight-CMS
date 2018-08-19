4aa84ffdaee16a203d91519ce154da1230959c6f
jkorth <jake@modinfinity.com>
2018-08-19 15:13:38 -0500

    Add dependencies

--- mix.exs
+++ mix.exs
@@ -6,9 +6,9 @@ defmodule DarkfightCms.Mixfile do
       app: :darkfight_cms,
       version: "0.0.1",
       elixir: "~> 1.4",
-      elixirc_paths: elixirc_paths(Mix.env),
-      compilers: [:phoenix, :gettext] ++ Mix.compilers,
-      start_permanent: Mix.env == :prod,
+      elixirc_paths: elixirc_paths(Mix.env()),
+      compilers: [:phoenix, :gettext] ++ Mix.compilers(),
+      start_permanent: Mix.env() == :prod,
       aliases: aliases(),
       deps: deps()
     ]
@@ -26,7 +26,7 @@ defmodule DarkfightCms.Mixfile do
 
   # Specifies which paths to compile per environment.
   defp elixirc_paths(:test), do: ["lib", "test/support"]
-  defp elixirc_paths(_),     do: ["lib"]
+  defp elixirc_paths(_), do: ["lib"]
 
   # Specifies your project dependencies.
   #
@@ -40,7 +40,15 @@ defmodule DarkfightCms.Mixfile do
       {:phoenix_html, "~> 2.10"},
       {:phoenix_live_reload, "~> 1.0", only: :dev},
       {:gettext, "~> 0.11"},
-      {:cowboy, "~> 1.0"}
+      {:cowboy, "~> 1.0"},
+      {:phauxth, "~> 1.2"},
+      {:argon2_elixir, "~> 1.2"},
+      {:bamboo, "~> 1.0"},
+      {:dialyxir, "~>0.5", only: [:dev], runtime: false},
+      {:distillery, "~> 1.5", runtime: false},
+      {:timex, "~> 3.2"},
+      {:credo, "~> 0.9", only: [:dev, :test], runtime: false},
+      {:sobelow, "~> 0.6", only: [:dev, :test]}
     ]
   end
 
@@ -54,7 +62,7 @@ defmodule DarkfightCms.Mixfile do
     [
       "ecto.setup": ["ecto.create", "ecto.migrate", "run priv/repo/seeds.exs"],
       "ecto.reset": ["ecto.drop", "ecto.setup"],
-      "test": ["ecto.create --quiet", "ecto.migrate", "test"]
+      test: ["ecto.create --quiet", "ecto.migrate", "test"]
     ]
   end
 end
